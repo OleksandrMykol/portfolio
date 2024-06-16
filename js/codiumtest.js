81ca489443e90a5a194e51de5ac6cee11f08c3d2
OLEK <demchenkos1980@gmail.com>
2024-06-16 12:53:47 +0300

    before change body flex-direction

--- js/codiumtest.js
+++ js/codiumtest.js
@@ -6,20 +6,22 @@ function incrementStepCount() {
 }
 
 function displayStepCount() {
-  const stepCounter = document.getElementById('step-counter');
+  const stepCounter = document.getElementById("step-counter");
   stepCounter.textContent = `Step ${stepCount}`;
 }
 
-const stepButton = document.getElementById('step-button');
-stepButton.addEventListener('click', incrementStepCount);
+const stepButton = document.getElementById("step-button");
+stepButton.addEventListener("click", incrementStepCount);
 
 // ?BUTTON CODIUM
 function toggleDarkMode() {
-  console.log('toggleDarkMode called');
-  document.body.classList.toggle('dark-mode');
+  console.log("toggleDarkMode called");
+  document.body.classList.toggle("dark-mode");
 }
-const darkModeBtn = document.getElementById('dark-mode-btn');
 
-darkModeBtn.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-});
+console.log("object");
+// const darkModeBtn = document.getElementById('dark-mode-btn');
+
+// darkModeBtn.addEventListener('click', () => {
+//   document.body.classList.toggle('dark-mode');
+// });
